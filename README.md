f59b8052879ed679ebbb2f83d113601bb1693cee
Andreas Seeger <splash@appfros.ch>
2022-12-29 10:15:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppfroschPersistence
+
+A description of this package.
---
+++ Sources/AppfroschPersistence/AppfroschPersistence.swift
@@ -0,0 +1,6 @@
+public struct AppfroschPersistence {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
